83a1d05b8f51984c9b4a583fba69a98483de0618
Justin Rains <justin@justinrains..com>
2017-05-06 15:51:49 -0400

    Initial code checkin

---
+++ src/questions.js
@@ -0,0 +1,124 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "What year was the general lee?": [
+                "1969",
+                "1970",
+                "1968",
+                "1971"
+            ]
+        },
+        {
+            "Bo and Luke were what?": [
+                "Cousins",
+                "Brothers",
+                "friends",
+                "prison inmates",
+            ]
+        },
+        {
+            "What is John Schneiders middle name?": [
+                "Richard",
+                "Tom",
+                "Joe",
+                "Dalton"
+            ]
+        },
+        {
+            "Who usually drove the General Lee?": [
+                "Bo",
+                "Luke",
+                "Uncle Jesse",
+                "Cooter"
+            ]
+        },
+        {
+            "Where was Sorrel Booke (Boss Hogg) born?": [
+                "Buffalo new york",
+                "los angeles california",
+                "austin texas",
+                "newark ohio"
+            ]
+        },
+        {
+            "What was boss hoggs real name?": [
+                "sorrel booke",
+                "wayne nelson",
+                "willie makeit",
+                "tom anderson"
+            ]
+        },
+        {
+            "What year did boss hogg die?": [
+                "1984",
+                "1980",
+                "1985",
+                "1987"
+            ]
+        },
+        {
+            "What did boss hogg drive?": [
+                "Cadillac",
+                "hyundai",
+                "ford",
+                "dodge"
+            ]
+        },
+        {
+            "What is Daisy Dukes real name?": [
+                "Catherine Bach",
+                "Kaitlyn Jenner",
+                "Sallie Mae",
+                "Mary Gonzalez"
+            ]
+        },
+        {
+            "Where was catherine bach born?": [
+                "warren ohio",
+                "mason ohio",
+                "lebanon ohio",
+                "bethel ohio"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_GB" : [
+        {
+            "What year was the general lee?": [
+                "1969",
+                "1970",
+                "1968",
+                "1971"
+            ]
+        },
+        {
+            "Bo and Luke were ": [
+                "Cousins",
+                "Brothers",
+                "friends",
+                "prison inmates",
+            ]
+        },
+        {
+            "What is John Schneiders middle name?": [
+                "Richard",
+                "Tom",
+                "Joe",
+                "Dalton"
+            ]
+        },
+        {
+            "Who usually drove the General Lee?": [
+                "Bo",
+                "Luke",
+                "Uncle Jesse",
+                "Cooter"
+            ]
+        }
+    ]
+};
